f2ca660a8ee2d54c1b9e0439d2b2c8302b4840e6
Vita Pluvia <vitapluvia@gmail.com>
2015-08-16 12:00:46 -0700

    fix(setup): small tweaks to setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+
+# Setup Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Setup fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+# Setup ZSH Deps
 wget https://raw.githubusercontent.com/trapd00r/utils/master/zsh_path -O /usr/local/bin/zsh_path
 chmod +x /usr/local/bin/zsh_path
+
+# Setup Vim-Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Change current shell to ZSH
+chsh -s /bin/zsh
+
+
