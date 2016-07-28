e7c4c2f9650e86154ccf600edd9534aff2b3e00b
vitapluvia <vitapluvia@gmail.com>
2016-07-27 17:38:04 -0700

    feat(setup): add more linux setup

--- setup.sh
+++ setup.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
-# Setup Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp \.* ~/
+mkdir -p ~/.vim/swap ~/.vim/backup ~/.vim/undo
 
-# Make backup, swap & undo directories for vim
-mkdir ~/.vim/backup ~/.vim/swap ~/.vim/undo
+# Remove zeitgeist
+sudo apt-get -y --purge remove libzeitgeist* *-zeitgeist zeitgeist-*
+ps uax | grep -i zeit | grep -vi grep | awk '{print $2}' | xargs kill -9
+rm -rf ~/.local/share/zeitgeist
 
-# Setup fzf
+# Chrome setup
+sed -i 's/@include common-session/session optional pam_xauth.so\n\0/' /etc/pam.d/su
+useradd -m chrome
+usermod -s /bin/false chrome
+
+# Standard tools
+apt-get install -y feh exiftool xmonad strace ltrace
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# r2
+git clone https://github.com/radare/radare2 ~/.r2-install
+~/.r2-install/sys/install.sh
+rm -rf ~/.r2-install
+
+# fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# Setup ZSH Deps
-wget https://raw.githubusercontent.com/trapd00r/utils/master/zsh_path -O /usr/local/bin/zsh_path
-chmod +x /usr/local/bin/zsh_path
+# vim-plugged
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Setup Vim-Plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# disable services
+service bluetooth stop
+service speech-dispatcher stop
+service apache-htcacheclean stop
+service rpcbind stop
 
-# Change current shell to ZSH
-chsh -s /bin/zsh
+# r2 further setup
+# touch ~/.radare2rc
+# [r2]: ec* > ~/.radare2rc
